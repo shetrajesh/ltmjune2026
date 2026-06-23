104ebd6cbde06873c0cf841167c6bc80c08adee4
rshet007@yahoo.com <rshet007@yahoo.com>
2026-06-23 09:26:02 +0530

    This is my first commit

---
+++ src/main/java/com/ltm/ltmjune2026/Ltmjune2026Application.java
@@ -0,0 +1,13 @@
+package com.ltm.ltmjune2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ltmjune2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ltmjune2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ltm/ltmjune2026/Ltmjune2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltm.ltmjune2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ltmjune2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
